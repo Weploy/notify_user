7ddb4b4e5dda138e5b9340dfc02fc6482eb9623a
Thil Bandara <tbandara@gmail.com>
2017-11-17 09:59:25 +1100

    Add logging to show notifications being sent
    This is so we have a bit of visibility of notifications being sent out

--- app/workers/notify_user/delivery_worker.rb
+++ app/workers/notify_user/delivery_worker.rb
@@ -12,6 +12,8 @@ module NotifyUser
         channel_options = delivery.notification.class.channels[channel_name.to_sym] || {}
         channel_class = (channel_name + "_channel").camelize.constantize
 
+        Rails.logger.info "Sending Notification #{delivery.notification.type} to: #{channel_name}"
+
         channel_class.deliver(delivery.notification_id, channel_options)
         delivery.update(sent_at: Time.zone.now)
       end
